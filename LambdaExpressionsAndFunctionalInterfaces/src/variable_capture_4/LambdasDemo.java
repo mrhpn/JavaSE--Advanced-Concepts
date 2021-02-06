6c63663915537af3b531f83c23e9a72a32bbf392
mrhpn <mr.htetphyonaing@gmail.com>
2021-02-06 18:25:50 +0630

    Variable Capture of Lambda Expressions

---
+++ LambdaExpressionsAndFunctionalInterfaces/src/variable_capture_4/LambdasDemo.java
@@ -0,0 +1,17 @@
+package variable_capture_4;
+
+public class LambdasDemo {
+	private static String subfix = "+";
+	
+	public static void show() {
+		String prefix = "-";
+		/**
+		 * Lambda Expression can access local variable and class's static and instance fields.
+		 */
+		greet(message -> System.out.println(prefix + message + subfix)); // Output: -Hello World+
+	}
+	
+	public static void greet(Printer printer) {
+		printer.print("Hello World");
+	}
+}
