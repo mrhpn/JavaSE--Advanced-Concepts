edd747be758c38edc08d231f57688c9e64ee0fee
mrhpn <mr.htetphyonaing@gmail.com>
2021-02-06 18:06:09 +0630

    Anonymous Inner Class

---
+++ LambdaExpressionsAndFunctionalInterfaces/src/anonymous_inner_class_2/LambdasDemo.java
@@ -0,0 +1,21 @@
+package anonymous_inner_class_2;
+
+public class LambdasDemo {
+	public static void show() {
+		/**
+		 * Anonymous Inner Class
+		 * ---
+		 * We don't need another seprate Console Printer class. In some cases, this might be useful.
+		 */
+		greet(new Printer() {
+			@Override
+			public void print(String message) {
+				System.out.println(message);
+			}
+		});
+	}
+	
+	public static void greet(Printer printer) {
+		printer.print("Hello World");
+	}
+}
---
+++ LambdaExpressionsAndFunctionalInterfaces/src/anonymous_inner_class_2/Main.java
@@ -0,0 +1,16 @@
+package anonymous_inner_class_2;
+
+/**
+ * Functional Interface
+ * -----
+ * An interface which has only a single abstract method
+ * Eg: Comparable, Comparator and so on...
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		LambdasDemo.show();
+	}
+
+}
