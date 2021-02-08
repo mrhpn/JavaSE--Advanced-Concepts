b862d7e3e81ec40d5d188b1015e776e38a39916a
mrhpn <mr.htetphyonaing@gmail.com>
2021-02-08 20:30:16 +0630

    Supplier interface

---
+++ LambdaExpressionsAndFunctionalInterfaces/src/a9_supplier_interface/LambdasDemo.java
@@ -0,0 +1,23 @@
+package a9_supplier_interface;
+
+import java.util.function.Supplier;
+
+/**
+ * Supplier Interface
+ * -----
+ * Documentation: https://docs.oracle.com/javase/8/docs/api/java/util/function/Supplier.html
+ * 
+ * Supplier interface is a functional interface because it has a single abstract method, `T get()`.
+ * So, we can represent it as a lambda expression.
+ *
+ */
+public class LambdasDemo {
+	public static void show() {
+		// When we run the program, this lambda expression will not immediately run unless we don't explicity call it, like getRandom.get().
+		// This is called Lazy-Evaluation.
+		// If you are working with premitive types, you should use primitive suppliers like `DoubleSupplier`, `IntSupplier`, `BooleanSupplier` and so on.
+		Supplier<Double> getRandom = () -> Math.random();
+		var random = getRandom.get(); // When we call supplier interface, we should call like this.
+		System.out.println(random);
+	}
+}
---
+++ LambdaExpressionsAndFunctionalInterfaces/src/a9_supplier_interface/Main.java
@@ -0,0 +1,9 @@
+package a9_supplier_interface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LambdasDemo.show();
+	}
+
+}
